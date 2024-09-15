9cdec5f9de976066f4aca6b5388c57f5d2a4903d
Polidowze <167921430+Polidowze@users.noreply.github.com>
2024-09-15 12:28:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Globalization;
+
+string Palavra;
+
+
+Console.WriteLine("Digite uma Palavra desejada");
+Palavra = Console.ReadLine()!;
+Console.WriteLine(Palavra);
+Console.WriteLine(Palavra);
